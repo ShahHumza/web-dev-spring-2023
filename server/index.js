45a6ec9f3cc880a34ee126322876826b7993e15b
HumzaShah <humzashah1001@gmail.com>
2023-02-13 15:11:45 -0500

    Package.json File

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const server = http.createServer((req, res) => {
 
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello NewPlatz');
+  res.end('Hello New Paltz');
 
 });
 

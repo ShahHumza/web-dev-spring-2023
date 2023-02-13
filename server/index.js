df1e79108b33b3188089c8f52a36cb6ee931e62c
HumzaShah <humzashah1001@gmail.com>
2023-02-13 15:00:09 -0500

    index.js file

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = process.env.PORT || 3000; //change this if you have an error
+
+const server = http.createServer((req, res) => {
+
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello NewPlatz');
+
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});

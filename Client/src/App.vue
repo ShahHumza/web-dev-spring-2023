f0405b5f1c90890fb405a010a1544f79b89cae1f
HumzaShah <humzashah1001@gmail.com>
2023-02-27 15:14:18 -0500

    2-2-7-23 In class implement html css with Vue

--- Client/src/App.vue
+++ Client/src/App.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import NavBar from './components/NavBar.vue';
+
 // import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
+  <NavBar />
+
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 

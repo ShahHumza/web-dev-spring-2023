3e7fffb0dd23ebb3cc56d57d5aeb20ba86e7cad4
HumzaShah <humzashah1001@gmail.com>
2023-03-02 14:28:05 -0500

    updated vue

--- Client/src/App.vue
+++ Client/src/App.vue
@@ -2,26 +2,17 @@
 import { RouterLink, RouterView } from 'vue-router'
 import NavBar from './components/NavBar.vue';
 
-// import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
-  <NavBar />
+    
+    <NavBar />
 
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <!-- <HelloWorld msg="New Paltz!" /> -->
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+    <div class="container">
+        <RouterView />
     </div>
-  </header>
-
-  <RouterView />
+    
 </template>
 
 <style scoped>

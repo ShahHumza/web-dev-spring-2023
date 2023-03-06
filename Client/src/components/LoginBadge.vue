1128c79c1716be9f53824341e326122851478cc1
HumzaShah <humzashah1001@gmail.com>
2023-03-06 14:09:50 -0500

    Login Working

--- Client/src/components/LoginBadge.vue
+++ Client/src/components/LoginBadge.vue
@@ -1,43 +1,32 @@
 <script setup lang="ts">
-import { useSession, login } from '@/model/session';
 import { ref } from 'vue';
-
-
+import { useSession, login } from '@/model/session';
 const session = useSession();
-
 </script>
 
 <template>
-    
-        <div class="navbar-item" v-if="session == null">
-            Welcome, {{ 'Humza Shah' }}
-            (<a @click="$event => session.user = null">logout</a>)
-              <a class="button is-primary" @click="login">
-                <span class="icon">
-                  <i class="fas fa-user">
-
-                  </i>
-                </span>
-                <strong> Sign Up</strong>
-    
-              </a>
+            <div class="navbar-item" v-if="session.user">
+                Welcome, {{ session.user.name }}
+                (<a @click=" session.user = null">logout</a>)
             </div>
             <div class="navbar-item" v-else>
-              <a class="button is-light">
-                <span class="icon">
-                  <i class="fas fa-user">
-
-                  </i>
-                </span>
-                <strong> Login In</strong>
-    
-              </a>
+                <a class="button is-primary" @click="login">
+                    <span class="icon">
+                        <i class="fas fa-user"></i>
+                    </span>
+                    <strong>Login</strong>
+                </a>
+                <a class="button is-info">
+                    <span class="icon">
+                        <i class="fas fa-user-plus"></i>
+                    </span>
+                    <strong>Sign up</strong>
+                </a>
             </div>
-    
+
+
+
 </template>
 
-
-
 <style scoped>
-
 </style>
